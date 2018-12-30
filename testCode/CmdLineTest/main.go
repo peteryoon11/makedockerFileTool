4f254b5d3718d7628b2654475195ca533afd6248
peteryoon11 <yjisub11@gmail.com>
2018-12-30 09:07:22 +0900

    cmd arrays test for exec.command

--- testCode/CmdLineTest/main.go
+++ testCode/CmdLineTest/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	temp := make([]string, 0)
-	fmt.Println(temp)
+	/* temp := make([]string, 0)
+	fmt.Println(temp) */
+	testString := "docker run --rm -it ubuntu:16.04 /bin/bash"
+	for num, item := range strings.Split(testString, " ") {
+		fmt.Println(num, item)
+	}
 }
