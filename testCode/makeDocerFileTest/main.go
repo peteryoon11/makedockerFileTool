f32fb8d7193b55cb1b301ef3d67270c132249e31
peteryoon11 <yjisub11@gmail.com>
2019-01-01 14:02:02 +0900

    modifiy docker file

--- testCode/makeDocerFileTest/main.go
+++ testCode/makeDocerFileTest/main.go
@@ -6,9 +6,12 @@ func main() {
 		from
 		-> 어느 도커 이미지를 기반으로 만들건지
 		maintainer
-		-> 관리자 
-		add 
-		-> 현재 나의 공간에서 도커 이미지 내부로 추가할 파일들 
-		
+		-> 관리자
+		add
+		-> 현재 나의 공간에서 도커 이미지 내부로 추가할 파일들
+
 	*/
+	// sudo 를 붙여야 하는 경우에 대해서도 고려 해야 함
+	//docker run --rm -it ubuntu:16.04 /bin/bash
+	//docker run ubuntu:16.04
 }
